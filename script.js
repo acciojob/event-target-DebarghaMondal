8f4e3c0aaf69e06fc78b72020843ad4428858bd8
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-31 09:38:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,33 @@ class EventTarget {
     }
   
     addEventListener(event, callback) {
-      
+      // If event already exists in the map
+      if (this.listeners.has(event)) {
+        // Get the existing array and add the new callback
+        this.listeners.get(event).push(callback);
+      } else {
+        // Else, add new event with the callback function in an array
+        this.listeners.set(event, [callback]);
+      }
     }
   
     removeEventListener(event, callback) {
-      
+      // If event exists
+      if (this.listeners.has(event)) {
+        // Get the array of callbacks
+        let listeners = this.listeners.get(event);
+        // Filter out the specific callback
+        listeners = listeners.filter(listener => listener !== callback);
+        // Update the listeners map
+        this.listeners.set(event, listeners);
+      }
     }
   
     dispatchEvent(event) {
-    
+      // If event exists
+      if (this.listeners.has(event)) {
+        // Get the array of callbacks and execute each one
+        this.listeners.get(event).forEach(callback => callback());
+      }
     }
-    
-  }
-  
+}
